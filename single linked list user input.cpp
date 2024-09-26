0f8e012f96999e990f3ca82a3a2bbc0432212ccf
idfc xing <154302338+ibmrafi69@users.noreply.github.com>
2024-09-26 19:53:46 +0600

    singly linked list user input.cpp

--- single linked list user input.cpp
+++ single linked list user input.cpp
@@ -1,53 +1,42 @@
 #include<iostream>
-#include<cstdlib>
 using namespace std;
-struct node
+class node
 {
+    public:
     int data;
-    node *link;
+    node*link;
 };
-node*create_node_in_linked_list(node*head)
+int main()
 {
-    int data;
-    cout<<"Enter your data:";
-    cin>>data;
-    node*p=(node*)malloc(sizeof(node));
-    p->data=data;
-    p->link=NULL;
-    node*ptr;
-    if(head==NULL)
+    int n;
+    cout<<"Enter your node number:";
+    cin>>n;
+    node*head=NULL;
+    for(int i=0;i<n;i++)
     {
-        head=p;
-        ptr=head;
-        return head;
+        int data;
+        cout<<"Enter your data:";
+        cin>>data;
+        node*p=new node();
+        p->data=data;
+        p->link=NULL;
+        node*ptr;
+        if(head==NULL)
+        {
+            head=p;
+            ptr=head;
+        }
+        else
+        {
+            ptr->link=p;
+            ptr=p;
+        }
     }
-    else
-    {
-        ptr->link=p;
-        ptr=p;
-        return head;
-    }
-}
-void print_linked_list(node*head)
-{
     node*p=head;
     while (p!=NULL)
     {
-        cout<<"Element is:"<<p->data<<endl;
+        cout<<p->data<<" ";
         p=p->link;
     }
-
-}
-int main()
-{
-   int n;
-   cout<<"Enter your number of node:";
-   cin>>n;
-   node*head=NULL;
-   for (int i = 0; i < n; i++)
-   {
-     head=create_node_in_linked_list(head);
-   }
-   print_linked_list(head);
-
+    return 0;
 }
