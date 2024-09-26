0f8f4de8fb8a97d047bedea1890e5bf7a299a399
idfc xing <154302338+ibmrafi69@users.noreply.github.com>
2024-09-26 21:40:21 +0600

    Add files via upload

---
+++ circle singly linked list insert node end of the list.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+class node
+{
+    public:
+    int data;
+    node*link;
+};
+node*head;
+int main()
+{
+    int n;
+    cout<<"Enter the number of nodes: ";
+    cin>>n;
+    node*tail;
+    for(int i=1;i<=n;i++)
+    {
+        int data;
+        cout<<"Enter the data for node "<<i<<": ";
+        cin>>data;
+        node*nn=new node();
+        nn->data=data;
+        nn->link=NULL;
+        if(head==NULL)
+        {
+            head=nn;
+            tail=head;
+            tail->link=head;
+        }
+        else
+        {
+            tail->link=nn;
+            tail=nn;
+            tail->link=head;
+        }
+    }
+    node*p=head;
+    do
+    {
+        cout<<p->data<<" ";
+        p=p->link;
+    } while (p!=head);
+    cout<<endl;
+    int dataa;
+    cout<<"Enter the data to be added:";
+    cin>>dataa;
+    node*nn=new node();
+    nn->data=dataa;
+    nn->link=head;
+    tail->link=nn;
+    tail=nn;
+    cout<<"After insert node"<<endl;
+    node*x=head;
+    do
+    {
+        cout<<x->data<<" ";
+        x=x->link;
+    } while (x!=head);
+
+}
