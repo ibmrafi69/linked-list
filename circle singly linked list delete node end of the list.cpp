76df45e1c4c17ed3e0ff175b09e272a76d771b19
idfc xing <154302338+ibmrafi69@users.noreply.github.com>
2024-09-26 21:55:07 +0600

    Add files via upload

---
+++ circle singly linked list delete node end of the list.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+class node
+{
+    public:
+    int data;
+    node*link;
+};
+node*head;
+int main()
+{
+    int n;
+    cout<<"Enter the number of nodes: ";
+    cin>>n;
+    node*tail;
+    for(int i=1;i<=n;i++)
+    {
+        int data;
+        cout<<"Enter the data for node "<<i<<": ";
+        cin>>data;
+        node*nn=new node();
+        nn->data=data;
+        nn->link=NULL;
+        if(head==NULL)
+        {
+            head=nn;
+            tail=head;
+            tail->link=head;
+        }
+        else
+        {
+            tail->link=nn;
+            tail=nn;
+            tail->link=head;
+        }
+    }
+    node*p=head;
+    do
+    {
+        cout<<p->data<<" ";
+        p=p->link;
+    } while (p!=head);
+    cout<<endl;
+    node*temp=tail;
+    node*temp1=tail;
+    do
+    {
+        temp1=temp;
+        temp=temp->link;
+    }while(temp!=tail);
+    temp1->link=temp->link;
+    free(temp);
+    cout<<"After delete"<<endl;
+    node*x=head;
+    do
+    {
+        cout<<x->data<<" ";
+        x=x->link;
+    } while (x!=head);
+}
